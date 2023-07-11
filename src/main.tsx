9a0e7acbc0b5c36370c5b013ddb32fcf2e98732c
Seb Dybowski <dybowski.seb@gmail.com>
2023-07-11 10:24:05 +0200

    pwa config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  async onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      await updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("offline ready");
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -12,24 +12,44 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: [
+        "**/*",
+      ],
       manifest: {
-        name: 'My Awesome App',
-        short_name: 'MyApp',
-        description: 'My Awesome App description',
-        theme_color: '#ffffff',
-        icons: [
+        "theme_color": "#f69435",
+        "background_color": "#f69435",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "short_name": "Vite PWA",
+        "description": "Vite PWA Boilerplate",
+        "name": "Vite PWA Boilerplate",
+        "icons": [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ]
-      }
+      },
     })
   ]
 })
